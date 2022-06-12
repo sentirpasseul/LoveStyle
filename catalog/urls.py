07d50db8f7767d0a1e16c4478e0f979bf832d1ab
sentirpasseul <71366294+sentirpasseul@users.noreply.github.com>
2022-06-12 12:08:26 +0800

    Добавлен статический файл стиля Добавлено динамическое отображение каждого файла по шаблону basic.html Первая глава книги завершена

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
 from catalog.views import index
-from .views import index, by_type
+from .views import index, by_type, ItemCreateView
 
 urlpatterns = [
-    path('<int:type_id>/', by_type),
-    path('', index)
+    path('add/', ItemCreateView.as_view(), name='add'),
+    path('<int:type_id>/', by_type, name='by_type'),
+    path('', index, name='index')
 ]
